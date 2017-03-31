bfa0e31ebebb12738fa6f990200445d9b26e8f0c
Pamela Munghen <pammunghen@users.noreply.github.com>
2017-03-31 00:41:46 -0400

    Add files via upload
    This is an idea of how we can use the perl/R script to produce a line graph of the parsed data.

---
+++ plottry.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+#
+#   Packages and modules
+#
+use strict;
+use warnings;
+use version;         our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
+use Statistics::R;
+
+my $infilename;
+my $pdffilename;
+#my $graphname;
+#my $length;
+
+#
+#   Check that you have the right number of parameters
+#
+if ($#ARGV != 1 ) {
+   print "Usage: plotCrime.pl <input file name> <pdf file name>\n" or
+      die "Print failure\n";
+   exit;
+} else {
+   $infilename = $ARGV[0];
+   $pdffilename = $ARGV[1];
+}  
+
+print "input file = $infilename\n";
+print "pdf file = $pdffilename\n";
+
+#Hoping to use the below and the if/else to title the graph: unclear if this is possible
+
+#print "What is the title of your graph?";
+#$graphname = <STDIN>;
+
+#print $graphname;
+
+#$length = length( $graphname );
+
+#if($length >= 1)
+#{
+
+    # Create a communication bridge with R and start R
+    my $R = Statistics::R->new();
+
+    # Name the PDF output file for the plot  
+    #my $Rplots_file = "./Rplots_file.pdf";
+
+    # Set up the PDF file for plots
+    $R->run(qq`pdf("$pdffilename" , paper="letter")`);
+
+    # Load the plotting library
+    $R->run(q`library(ggplot2)`);
+
+    # read in data from a CSV file
+    $R->run(qq`data <- read.csv("$infilename")`);
+
+    # plot the data as a line plot with each point outlined
+    $R->run(q`ggplot(data, aes(x=Year, y=Incidents, colour=Crime, group=Crime)) + geom_line() + geom_point(size=2) + ggtitle("Your Graph")`);
+    # close down the PDF device
+    $R->run(q`dev.off()`);
+
+    $R->stop();
+#}else{
+ #   print "Try again."
+  #  }
