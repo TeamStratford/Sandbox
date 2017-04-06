7beb3b356175e02ea0b86e23fbb41a0b2cf67fe9
Elvis Nguyen <nguyene@mail.uoguelph.ca>
2017-04-06 11:55:07 -0400

    Add files via upload
    - Added support for statistics column
    - Converted all values to lowercase to simplify string comparison
        Note: I looked into a string similarity library that could be useful to us.
    Bug fixes: 
    - No more leading 6s show up on the first line of the CSV file
    - CSV headers no longer repeat theirselves throughout the CSV

--- index.pl
+++ index.pl
@@ -6,9 +6,8 @@ use strict;
 use warnings;
 use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
 use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher)
+use feature qw( unicode_strings );
 
-my $EMPTY = q{};
-my $SPACE = q{ };
 my $COMMA = q{,};
 
 my $csv = Text::CSV->new({ sep_char => $COMMA});
@@ -41,6 +40,17 @@ my %geo_line_end;
 my %violation_index;
 my %violation_line_start;
 my %violation_line_end;
+
+my %statistic_index;
+my %statistic_line_start;
+my %statistic_line_end;
+
+open my $fp_save_geo, '>>', "save_geo_".$file_name or die "Cannot open save file";
+
+open my $fp_save_violation, '>>', "save_violation_".$file_name or die "Cannot open save file";
+
+open my $fp_save_statistic, '>>', "save_statistic_".$file_name or die "Cannot open save file";
+
 foreach my $line (@data)
 {
 	if($csv->parse( $line))
@@ -77,56 +87,90 @@ foreach my $line (@data)
 
 		my $previous_location = 0;
 		my $previous_violation = 0;
-
-		open my $fp_save_geo, '>>', "save_geo_".$file_name
-		or die "Cannot open save file";
-
-		open my $fp_save_violation, '>>', "save_violation_".$file_name
-		or die "Cannot open save file";
+		my $previous_statistic = 0;
 
 		for(0 .. $column_counter)
 		{
-			if(exists $geo_index{$data[$geo_col]})
+			my $location = lc($data[$geo_col]);
+			my $violation = lc($data[$violation_col]);
+			my $statistic = lc($data[$statistic_col]);
+
+			if(exists $geo_index{$location})
 			{
-				if(exists $violation_index{$data[$geo_col]}{$data[$violation_col]})
+
+				if(exists $violation_index{$location}{$violation})
 				{
-					
+					if(exists $statistic_index{$location}{$violation}{$statistic})
+					{
+
+					}
+					else
+					{
+						if($statistic ne "sta")
+						{
+							if(%statistic_index and $previous_statistic ne $statistic)
+							{
+								$statistic_line_end{$data[$previous_statistic]} = $row_counter - 1;
+								print $fp_save_statistic $statistic_line_end{$data[$previous_statistic]}."\n";
+							}
+
+							my @tokens = split(/\./, $data[$coordinate_col]);
+							$statistic_index{$location}{$violation}{$statistic} = $tokens[2];
+
+							if($statistic eq "actual incidents" and $violation eq "total, all violations" and $location eq "canada")
+							{
+								print $fp_save_statistic '"'."location".'"'.",".'"'."violation".'"'.",".'"'."statistic".'"'.','.'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
+								$statistic_line_start{$statistic} = $row_counter-5;
+							}
+							else
+							{
+								$statistic_line_start{$statistic} = $row_counter;
+							}
+							print $fp_save_statistic '"'.$location.'"'.",";
+							print $fp_save_statistic '"'.$violation.'"'.",";
+							print $fp_save_statistic '"'.$statistic.'"'.",";
+							print $fp_save_statistic $statistic_index{$location}{$violation}{$statistic}.",";
+							print $fp_save_statistic $statistic_line_start{$statistic}.",";
+							$previous_statistic = $statistic_col;
+						}	
+					}
 				}
 				else
 				{
-					if($data[$violation_col] ne "VIOLATIONS")
+					if($violation ne "violations")
 					{
-						if(%violation_index and $previous_violation ne $data[$violation_col])
+						if(%statistic_index and $previous_violation ne $violation)
 						{
 							$violation_line_end{$data[$previous_violation]} = $row_counter - 1;
 							print $fp_save_violation $violation_line_end{$data[$previous_violation]}."\n";
 						}
 
 						my @tokens = split(/\./, $data[$coordinate_col]);
-						$violation_index{$data[$geo_col]}{$data[$violation_col]} = $tokens[1];
+						$violation_index{$location}{$violation} = $tokens[1];
 
-						if($data[$violation_col] eq "Total, all violations")
+						if($violation eq "total, all violations" and $location eq "canada")
 						{
 							print $fp_save_violation '"'."location".'"'.",".'"'."violation".'"'.",".'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
-							$violation_line_start{$data[$violation_col]} = $row_counter-5;
+							$violation_line_start{$violation} = $row_counter-5;
 						}
 						else
 						{
-							$violation_line_start{$data[$violation_col]} = $row_counter;
+							$violation_line_start{$violation} = $row_counter;
 						}
-						print $fp_save_violation '"'.$data[$geo_col].'"'.",";
-						print $fp_save_violation '"'.$data[$violation_col].'"'.",";
-						print $fp_save_violation $violation_index{$data[$geo_col]}{$data[$violation_col]}.",";
-						print $fp_save_violation $violation_line_start{$data[$violation_col]}.",";
+						print $fp_save_violation '"'.$location.'"'.",";
+						print $fp_save_violation '"'.$violation.'"'.",";
+						print $fp_save_violation $violation_index{$location}{$violation}.",";
+						print $fp_save_violation $violation_line_start{$violation}.",";
 						$previous_violation = $violation_col;
 					}
 				}	
 			}
 			else
 			{
-				if($data[$geo_col] ne "GEO")
+				if($location ne "geo")
 				{
-					if(%geo_index and $previous_location ne $data[$geo_col])
+
+					if(%violation_index and $previous_location ne $location)
 					{
 						$geo_line_end{$data[$previous_location]} = $row_counter - 1;
 						print $fp_save_geo $geo_line_end{$data[$previous_location]};
@@ -136,7 +180,7 @@ foreach my $line (@data)
 					my @tokens = split(/\./, $data[$coordinate_col]);
 
 					#Add ID to string location
-					$geo_index{$data[$geo_col]} = $tokens[0];
+					$geo_index{$location} = $tokens[0];
 				
 					#Assigns start line number (from the csv) to ID
 					#
@@ -146,26 +190,30 @@ foreach my $line (@data)
 					if($data[$geo_col] eq "Canada")
 					{
 						print $fp_save_geo '"'."geo_loc".'"'.",".'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
-						$geo_line_start{$data[$geo_col]} = $row_counter-5;
+						$geo_line_start{$location} = $row_counter-5;
 					}
 					else
 					{
-						$geo_line_start{$data[$geo_col]} = $row_counter;
+						$geo_line_start{$location} = $row_counter;
 						print $fp_save_geo "\n";
 					}
 
-					print "Loaded: ".$data[$geo_col]."\n";
+					print "Loaded: ".$location."\n";
 
-					print $fp_save_geo '"'.$data[$geo_col].'"'.",";
-					print $fp_save_geo $geo_index{$data[$geo_col]}.",";
-					print $fp_save_geo $geo_line_start{$data[$geo_col]}.",";
+					print $fp_save_geo '"'.$location.'"'.",";
+					print $fp_save_geo $geo_index{$location}.",";
+					print $fp_save_geo $geo_line_start{$location}.",";
 					$previous_location = $geo_col;
 				}
 			}
 
 		}
-		close $fp_save_geo;
 		$row_counter++;
 	}
-	
 }
+print $fp_save_geo $row_counter."\n";
+print $fp_save_violation $row_counter."\n";
+print $fp_save_statistic $row_counter."\n";
+close $fp_save_geo;
+close $fp_save_violation;
+close $fp_save_statistic;
