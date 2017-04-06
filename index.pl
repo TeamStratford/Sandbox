07392619633d55c3a812a620d27c8420ceff613b
Elvis Nguyen <nguyene@mail.uoguelph.ca>
2017-04-06 18:24:48 -0400

    Add files via upload
    Bug fixes:
    - Previous location, violation and statistic variable scope fixed.
    
    Known issues:
    -Unicode characters are not parsed by Text::CSV
    
    What's new:
    - Single string search

--- index.pl
+++ index.pl
@@ -9,24 +9,26 @@ use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher
 use feature qw( unicode_strings );
 
 my $COMMA = q{,};
-
 my $csv = Text::CSV->new({ sep_char => $COMMA});
 
-
-
+#For the purposes of the demo and time constraints, we hardcoded the filename to
+#prevent the user from inputting invalid filenames.
+#This functionality can be added by prompting the user for input and using the following:
+#  while (!(-e $file_name))
+#  {
+#
+#  }
 my $file_name = "load.csv";
+
 my @data;
 my $row_counter = 1;
-
+my $line_counter = 1;
 open my $fileHandler, '<', $file_name
 or die "Unable to open file: $file_name\n";
 
 	@data = <$fileHandler>;
-
 close $fileHandler
 or die "Unable to close fileHandler\n";
-
-
 
 my $coordinate_col = 0;
 my $geo_col = 0;
@@ -45,19 +47,55 @@ my %statistic_index;
 my %statistic_line_start;
 my %statistic_line_end;
 
+my @year;
+my @geo;
+my @vio;
+my @sta;
+my @vect;
+my @coord;
+my @val;
+#####################init()#####################
+# Description: Loads the crime data CSV to memory and loads the values 
+#              of the columns into an associaive array if all save files
+#              do not already exist. 
+#	       
+#              The hash values are based off of the coordinate column of 
+#              the CSV file for 'index' vars. Each column will also have
+#              a hash to store each start and end line number.
+#
+# Usage:       init();
+#
+# Preconditions: Crime data CSV file exists and atleast one of the save files
+#		 are not present.
+#
+# Postconditions: Coordinate values are assigned to each column by assiociating
+#		  string values into a associative arrays. Start lines and end lines of 
+#		  each combination are stored into separate associative arrays.
+sub init 
+{
 open my $fp_save_geo, '>>', "save_geo_".$file_name or die "Cannot open save file";
 
 open my $fp_save_violation, '>>', "save_violation_".$file_name or die "Cannot open save file";
 
 open my $fp_save_statistic, '>>', "save_statistic_".$file_name or die "Cannot open save file";
 
+my $previous_location = "canada";
+my $previous_violation = "total, all violations";
+my $previous_statistic = "actual incidents";
+
 foreach my $line (@data)
 {
 	if($csv->parse( $line))
         {
 		my @load_masterfields = $csv->fields();
-
-
+                $year[$line_counter] = $load_masterfields[0];
+                $geo[$line_counter] = $load_masterfields[1];
+                $vio[$line_counter] = $load_masterfields[2];
+		$sta[$line_counter] = $load_masterfields[3];
+		$vect[$line_counter] = $load_masterfields[4];
+		$coord[$line_counter] = $load_masterfields[5];
+		$val[$line_counter] = $load_masterfields[6];
+		$line_counter++;
 		my $column_counter = 0;
 		#This loop counts the number of columns and tracks values for the coordinates
 		
@@ -84,11 +122,6 @@ foreach my $line (@data)
 		}
 
 
-
-		my $previous_location = 0;
-		my $previous_violation = 0;
-		my $previous_statistic = 0;
-
 		for(0 .. $column_counter)
 		{
 			my $location = lc($data[$geo_col]);
@@ -110,8 +143,8 @@ foreach my $line (@data)
 						{
 							if(%statistic_index and $previous_statistic ne $statistic)
 							{
-								$statistic_line_end{$data[$previous_statistic]} = $row_counter - 1;
-								print $fp_save_statistic $statistic_line_end{$data[$previous_statistic]}."\n";
+								$statistic_line_end{$previous_location}{$previous_violation}{$previous_statistic} = $row_counter - 1;
+								print $fp_save_statistic $statistic_line_end{$previous_location}{$previous_violation}{$previous_statistic}."\n";
 							}
 
 							my @tokens = split(/\./, $data[$coordinate_col]);
@@ -120,18 +153,18 @@ foreach my $line (@data)
 							if($statistic eq "actual incidents" and $violation eq "total, all violations" and $location eq "canada")
 							{
 								print $fp_save_statistic '"'."location".'"'.",".'"'."violation".'"'.",".'"'."statistic".'"'.','.'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
-								$statistic_line_start{$statistic} = $row_counter-5;
+								$statistic_line_start{$location}{$violation}{$statistic} = $row_counter-5;
 							}
 							else
 							{
-								$statistic_line_start{$statistic} = $row_counter;
+								$statistic_line_start{$location}{$violation}{$statistic} = $row_counter;
 							}
 							print $fp_save_statistic '"'.$location.'"'.",";
 							print $fp_save_statistic '"'.$violation.'"'.",";
 							print $fp_save_statistic '"'.$statistic.'"'.",";
 							print $fp_save_statistic $statistic_index{$location}{$violation}{$statistic}.",";
-							print $fp_save_statistic $statistic_line_start{$statistic}.",";
-							$previous_statistic = $statistic_col;
+							print $fp_save_statistic $statistic_line_start{$location}{$violation}{$statistic}.",";
+							$previous_statistic = $statistic;
 						}	
 					}
 				}
@@ -141,8 +174,8 @@ foreach my $line (@data)
 					{
 						if(%statistic_index and $previous_violation ne $violation)
 						{
-							$violation_line_end{$data[$previous_violation]} = $row_counter - 1;
-							print $fp_save_violation $violation_line_end{$data[$previous_violation]}."\n";
+							$violation_line_end{$previous_location}{$previous_violation} = $row_counter - 1;
+							print $fp_save_violation $violation_line_end{$previous_location}{$previous_violation}."\n";
 						}
 
 						my @tokens = split(/\./, $data[$coordinate_col]);
@@ -151,17 +184,17 @@ foreach my $line (@data)
 						if($violation eq "total, all violations" and $location eq "canada")
 						{
 							print $fp_save_violation '"'."location".'"'.",".'"'."violation".'"'.",".'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
-							$violation_line_start{$violation} = $row_counter-5;
+							$violation_line_start{$location}{$violation} = $row_counter-5;
 						}
 						else
 						{
-							$violation_line_start{$violation} = $row_counter;
+							$violation_line_start{$location}{$violation} = $row_counter;
 						}
 						print $fp_save_violation '"'.$location.'"'.",";
 						print $fp_save_violation '"'.$violation.'"'.",";
 						print $fp_save_violation $violation_index{$location}{$violation}.",";
-						print $fp_save_violation $violation_line_start{$violation}.",";
-						$previous_violation = $violation_col;
+						print $fp_save_violation $violation_line_start{$location}{$violation}.",";
+						$previous_violation = $violation;
 					}
 				}	
 			}
@@ -172,8 +205,9 @@ foreach my $line (@data)
 
 					if(%violation_index and $previous_location ne $location)
 					{
-						$geo_line_end{$data[$previous_location]} = $row_counter - 1;
-						print $fp_save_geo $geo_line_end{$data[$previous_location]};
+		
+						$geo_line_end{$previous_location} = $row_counter - 1;
+						print $fp_save_geo $geo_line_end{$previous_location};
 					}
 
 					#Split coordinate from decimal values
@@ -190,7 +224,7 @@ foreach my $line (@data)
 					if($data[$geo_col] eq "Canada")
 					{
 						print $fp_save_geo '"'."geo_loc".'"'.",".'"'."id".'"'.",".'"'."start_line".'"'.",".'"'."end_line",'"'."\n";
-						$geo_line_start{$location} = $row_counter-5;
+						$geo_line_start{$location} = $row_counter;
 					}
 					else
 					{
@@ -203,7 +237,7 @@ foreach my $line (@data)
 					print $fp_save_geo '"'.$location.'"'.",";
 					print $fp_save_geo $geo_index{$location}.",";
 					print $fp_save_geo $geo_line_start{$location}.",";
-					$previous_location = $geo_col;
+					$previous_location = $location;
 				}
 			}
 
@@ -217,3 +251,103 @@ print $fp_save_statistic $row_counter."\n";
 close $fp_save_geo;
 close $fp_save_violation;
 close $fp_save_statistic;
+}
+
+sub searchGeo
+{
+	if(%geo_index)
+	{
+		my ($geo_location) = @_;
+		my $loc_copy = lc($geo_location);
+	
+		if(exists $geo_index{$loc_copy})
+		{
+			print $geo_index{$loc_copy}." ";
+			print $geo_line_start{$loc_copy}." ";
+			print $geo_line_end{$loc_copy}."\n";
+			readLines($geo_line_start{$loc_copy}, $geo_line_end{$loc_copy});
+		}
+	}
+	else
+	{
+		print "ERROR: geo_index not initialized\n";
+	}
+
+}
+sub searchVio
+{
+	if(%violation_index)
+	{
+		my ($vio_name) = @_;
+		my $vio_copy = lc($vio_name);
+		my @loc_keys = keys %geo_index;
+		while (@loc_keys)
+		{
+			my $loc_key = pop(@loc_keys);
+			if(exists $violation_index{$loc_key}{$vio_copy})
+			{
+				print $violation_index{$loc_key}{$vio_copy}." ";
+				print $violation_line_start{$loc_key}{$vio_copy}." ";
+				print $violation_line_end{$loc_key}{$vio_copy}."\n";
+				readLines($violation_line_start{$loc_key}{$vio_copy}, $violation_line_end{$loc_key}{$vio_copy});
+			}
+		}
+	}
+	else
+	{
+		print "ERROR: violation_index not initialized\n";
+	}
+
+}
+sub searchStat
+{
+	if(%statistic_index)
+	{
+		my ($stat_name) = @_;
+		my $stat_copy = lc($stat_name);
+
+		#Snippet adapted from a post by kennethk
+		#Reason: I didn't know how to get to the second level hashes
+		#
+		#URL: www.perlmonks.org/?node_id=824207
+		#Retreived on: April 6th, 2017 5:13PM EST
+		for my $primary_key (keys %violation_index)
+		{
+			my $child_key = $violation_index{$primary_key};
+			for my $secondary_key (keys %$child_key)
+			{
+				print $primary_key." ".$secondary_key."\n";
+				if(exists $statistic_index{$primary_key}{$secondary_key}{$stat_copy})
+				{
+					readLines($statistic_line_start{$primary_key}{$secondary_key}{$stat_copy}, $statistic_line_end{$primary_key}{$secondary_key}{$stat_copy});
+				}
+			}
+		}
+
+	}
+	else
+	{
+		print "ERROR: statistic_index not initialized\n";
+	}
+
+}
+sub readLines
+{
+	my ($minimum, $maximum) = @_;
+	$minimum = $minimum +5;
+	while ($minimum <= $maximum)
+	{
+                print $year[$minimum].",";
+                print $geo[$minimum].",";
+                print $vio[$minimum].",";
+		print $sta[$minimum].",";
+		print $vect[$minimum].",";
+		print $coord[$minimum].",";
+		print $val[$minimum]."\n";
+		$minimum++;
+	}
+}
+init();
+searchStat("actual incidents");
+readLines(2,5);
+
