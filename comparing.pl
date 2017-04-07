24a5a05f4eee5246235ba834ee19c6939b29b50e
Pamela Munghen <pammunghen@users.noreply.github.com>
2017-04-07 17:47:56 -0400

    Update comparing.pl

--- comparing.pl
+++ comparing.pl
@@ -4,24 +4,77 @@ use warnings;
 use version;   our $VERSION = qv('5.16.0'); 
 use Text::CSV;
  
-my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
+my $file = "output.csv" or die "Need to get CSV file on the command line\n";
 my $highest = 0;
 my $highestyear = 0;
+my $highestloc;
+my $lowest = 100;
+my $lowestyear = 0;
+my $lowestloc;
 my $compare;
 my $line;
+my $line_counter = 0;
+my $fp;
+my $total = 0;
+my $average = 0;
+
+my @data = {};
+my @year;
+my @geo;
+my @vio;
+my @sta;
+my @value;
+
+my $COMMA = q{,};
+my $csv = Text::CSV->new({ sep_char => $COMMA});
  
 my $recordcount = 0;
 
-open(my $data, '<', $file) or die "Could not open '$file' $!\n";
- 
-while ($line = <$data>) {
-  chomp $line;
-  my @fields = split ",", $line;
-  if($fields[2] > $highest){
-  	$highest = $fields[2];
-  	$highestyear = $fields[1];
-  }
+open($fp, '<', $file) or die "Could not open '$file' $!\n";
+ 	@data = <$fp>; 
+ close $fp or 
+ die "unable to close\n";
 
-  $recordcount++;
+foreach my $line (@data)
+{
+	if($csv->parse( $line))
+	{
+		my @load_masterfields = $csv->fields();
+		$line_counter++;
+	        $year[$line_counter] = $load_masterfields[0];
+	        $geo[$line_counter] = $load_masterfields[1];
+	        $vio[$line_counter] = $load_masterfields[2];
+		    $sta[$line_counter] = $load_masterfields[3];
+		    $value[$line_counter] = $load_masterfields[4];
+		    if ($value[$line_counter] > $highest){
+		    	$highest = $value[$line_counter];
+				$highestyear = $year[$line_counter];
+				$highestloc = $geo[$line_counter];
+			}
+			if ($value[$line_counter] < $lowest){
+		    	$lowest = $value[$line_counter];
+				$lowestyear = $year[$line_counter];
+				$lowestloc = $geo[$line_counter];
+
+			}
+
+			$total = $value[$line_counter] + $total;
+
+	}else {
+
+		print"ERROR";
+	}
 }
+
+print "FINALS\n";
+print "$line_counter\n";
 print "$highestyear\n";
+print "$lowestyear\n";
+print "$total\n";
+
+$average = $total / $line_counter;
+
+print "$average\n";
+
+print "$highestloc\n";
+print "$lowestloc\n";
